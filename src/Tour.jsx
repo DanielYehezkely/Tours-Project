4c3b1910de0bd6da3752be6c76a897119aee9a2e
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-05 15:19:12 +0300

    adding relevant files

---
+++ src/Tour.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Tour = () => {
+  return (
+    <h2>tour</h2>
+  )
+}
+export default Tour;
---
+++ src/Tours.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Tours = () => {
+  return (
+    <h2>Tours</h2>
+  )
+}
+export default Tours
