d3b098129db4c859f04f6973b9bd5364d5f529d8
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-05 18:39:24 +0300

    adding refresh fetched tours

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,17 @@ const removeTour = (id) => {
     </main>
   }
 
+  if(tours.length === 0){
+    return <main>
+      <div className="title">
+      <h2>
+        no tours left
+      </h2>
+        <button type="button" style={{marginTop: '2rem'}} className="btn" onClick={()=> fetchTours()}> Refresh Tours</button>
+      </div>
+    </main>
+  }
+
   return <main>
     <Tours tours={tours} removeTour={removeTour}/>
   </main>;
